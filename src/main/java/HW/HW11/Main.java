d7ef05ade10c7f2d24dc63f4d86c55e05fb5a626
Dmitry Pecheny <finman888@gmail.com>
2017-08-05 17:29:52 +0300

    HW11 task5

--- src/main/java/HW/HW11/Main.java
+++ src/main/java/HW/HW11/Main.java
@@ -10,30 +10,38 @@ public class Main {
         //Task 1
         sourceFile = FileUtils.createFile("task1.txt");
         resultFile = FileUtils.createFile("task1sortedInt.txt");
-
         FileUtils.generateRandomInt(sourceFile);
         TaskUtils.sortFileContents(sourceFile, resultFile);
 
         //Task 2
         sourceFile = FileUtils.createFile("task2.txt");
         resultFile = FileUtils.createFile("task2replacedWords.txt");
-
         FileUtils.writeFile(sourceFile, "publicsticvoidmail 1public1");
         TaskUtils.replaceWords(sourceFile, resultFile,"public", "private");
 
         //Task 3
         sourceFile = FileUtils.createFile("task3.txt");
         resultFile = FileUtils.createFile("task3reverseStrings.txt");
-
         FileUtils.writeFile(sourceFile, "abcdefABCDEF\n1234567890\n!@#$%^&*");
         TaskUtils.reverseByStrings(sourceFile, resultFile);
 
         //Task 4
         sourceFile = FileUtils.createFile("task4.txt");
         resultFile = FileUtils.createFile("task4UpperCase.txt");
-
         FileUtils.writeFile(sourceFile, "A a AA aa Aaa aAA\nAaAa    aAaA 12345");
         TaskUtils.fileToUpperCase(sourceFile, resultFile);
+
+        //Task 5
+        sourceFile = FileUtils.createFile("task5.txt");
+        resultFile = FileUtils.createFile("task5SurnameChange.txt");
+        FileUtils.writeFile(sourceFile, "Ivanov 4\nPetrov 8\nSidorov  10\n");
+        TaskUtils.surnameToUpperCase(sourceFile, resultFile);
+
+        //Task6
+        sourceFile = FileUtils.createFile("task6.txt");
+        resultFile = FileUtils.createFile("task6DetermineInput.txt");
+        FileUtils.writeFile(sourceFile, "a 111 bb 0 11.1 ccc 99.99 dddd ! @ # $ % ^ & *");
+        TaskUtils.determineInput(sourceFile, resultFile);
 
     }
 
--- src/main/java/HW/HW11/TaskUtils.java
+++ src/main/java/HW/HW11/TaskUtils.java
@@ -1,59 +1,93 @@
 package HW.HW11;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 class TaskUtils {
 
     //Task 1
-    static void sortFileContents(File sourceFile, File destinationFile) {
+    static void sortFileContents(File sourceFile, File resultFile) {
         String readedString = FileUtils.readFileToString(sourceFile);
         char[] chars = readedString.toCharArray();
         Arrays.sort(chars);
         String resultString = new String(chars);
 
-        FileUtils.writeFile(destinationFile, resultString);
+        FileUtils.writeFile(resultFile, resultString);
     }
 
     //Task 2
-    static void replaceWords(File sourceFile, File destinationFile, String sourceWord, String destinationWord){
-        String readedString = FileUtils.readFileToString(sourceFile);
-        String resultString = readedString.replaceAll(sourceWord, destinationWord);
+    static void replaceWords(File sourceFile, File resultFile, String sourceWord, String destinationWord){
+        String sourceString = FileUtils.readFileToString(sourceFile);
+        String resultString = sourceString.replaceAll(sourceWord, destinationWord);
 
-        FileUtils.writeFile(destinationFile, resultString);
+        FileUtils.writeFile(resultFile, resultString);
     }
 
     //Task 3
-    static void reverseByStrings(File file, File file2) {
-        List<String> sourceList = FileUtils.readFileToStringList(file);
+    static void reverseByStrings(File sourceFile, File resultFile) {
+        List<String> sourceList = FileUtils.readFileToStringList(sourceFile);
         StringBuilder stringBuilder = new StringBuilder();
         for (String s : sourceList) {
             stringBuilder.append(new StringBuilder(s).reverse() + "\n");
         }
 
-        FileUtils.writeFile(file2, stringBuilder.toString());
+        FileUtils.writeFile(resultFile, stringBuilder.toString());
     }
 
     //Task 4
-    static void fileToUpperCase(File file, File file2) {
-        List<String> stringList = FileUtils.readFileToStringList(file);
+    static void fileToUpperCase(File sourceFile, File resultFile) {
+        List<String> stringList = FileUtils.readFileToStringList(sourceFile);
         StringBuilder result = new StringBuilder();
-        for (int i = 0; i < stringList.size(); i++) {
-            Matcher m = Pattern.compile("\\b\\w{2,}\\b").matcher(stringList.get(i));
+        for (String string : stringList) {
+            Matcher m = Pattern.compile("\\w{2,}").matcher(string);
             StringBuilder sb = new StringBuilder();
             int last = 0;
             while (m.find()) {
-                sb.append(stringList.get(i).substring(last, m.start()));
+                sb.append(string.substring(last, m.start()));
                 sb.append(m.group(0).toUpperCase());
                 last = m.end();
             }
-            sb.append(stringList.get(i).substring(last));
+            sb.append(string.substring(last));
             result = result.append(sb + "\n");
         }
 
-        FileUtils.writeFile(file2, result.toString());
+        FileUtils.writeFile(resultFile, result.toString());
+    }
+
+    static void surnameToUpperCase(File sourceFile, File resultFile) {
+        List<String> stringList = FileUtils.readFileToStringList(sourceFile);
+        StringBuilder result = new StringBuilder();
+        for (String string : stringList) {
+            Matcher m = Pattern.compile("\\w{2,}[ \\-:]+[1]?[7-90]").matcher(string);
+            StringBuilder sb = new StringBuilder();
+            int last = 0;
+            while (m.find()) {
+                sb.append(string.substring(last, m.start()));
+                sb.append(m.group(0).toUpperCase());
+                last = m.end();
+            }
+            sb.append(string.substring(last));
+            result = result.append(sb + "\n");
+        }
+
+        FileUtils.writeFile(resultFile, result.toString());
+    }
+
+    static void determineInput(File sourceFile, File resultFile) {
+        String string = FileUtils.readFileToString(sourceFile);
+        List<Matcher> matcherList = new ArrayList<>();
+        matcherList.add(Pattern.compile(" \\w+ ").matcher(string));
+        matcherList.add(Pattern.compile(" \\p{Punct} ").matcher(string));
+        matcherList.add(Pattern.compile(" \\d+ ").matcher(string));
+        matcherList.add(Pattern.compile(" \\d+.\\d ").matcher(string));
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext(""))
+        System.out.println("Task 4. Input any letter/word/int digit/float digit\n" +
+                "to find matches form source file or input empty string to leave : ");
     }
 }
